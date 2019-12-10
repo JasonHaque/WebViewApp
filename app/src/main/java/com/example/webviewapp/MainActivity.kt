370c27bd5f5e4dcfe0759c8d4e139bfc768cf5dd
JasonHaque <zahinjason220434@gmail.com>
2019-12-10 14:43:39 +0600

    added web view

--- app/src/main/java/com/example/webviewapp/MainActivity.kt
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.webviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webview.loadUrl("https://github.com")
     }
 }
